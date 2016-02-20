183cd67eebcc776120652207c5f6ee233d4f3f1d
Will McMain <willmcmain@gmail.com>
2016-02-20 13:12:51 +0100

    basic input control & prototype

---
+++ Assets/Scripts/ControllerInput.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControllerInput : MonoBehaviour {
+	public float speed = 0.1f;
+
+	void Update () {
+		Vector3 p1 = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+		Vector3 p2 = new Vector3(Input.GetAxis("Horizontal2"), Input.GetAxis("Vertical2"));
+		float spd = Mathf.Min (p1.magnitude, p2.magnitude);
+
+		transform.position = transform.position + (p1+p2) * spd * speed;
+	}
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+	public GameObject sphere;
+	public GameObject target;
+	public Text winText;
+
+	private float inWin = 0f;
+	private float winDist = 0.3f;
+
+	void Start() {
+		sphere.transform.position = new Vector3 (Random.Range (-3, 3), Random.Range (-2, 2), 0);
+	}
+
+	void Update() {
+		Vector3 relative = (sphere.transform.position - target.transform.position);
+		if (relative.magnitude < winDist) {
+			inWin += Time.deltaTime;
+		} else {
+			inWin = 0;
+		}
+
+		if (relative.magnitude < winDist && inWin > 0.25) {
+			winText.enabled = true;
+		}
+
+		sphere.transform.position = new Vector3 (sphere.transform.position.x, sphere.transform.position.y, 0);
+	}
+}
