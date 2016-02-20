8e9e1709afe94d5681d9f69670b88b67afa2af4d
Will McMain <willmcmain@gmail.com>
2016-02-20 15:11:05 +0100

    InControl library

--- Assets/Scripts/ControllerInput.cs
+++ Assets/Scripts/ControllerInput.cs
@@ -1,13 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
+using InControl;
 
 public class ControllerInput : MonoBehaviour {
 	public float speed = 0.1f;
 
+	private InputDevice controller; 
+	void Start() {
+
+	}
+
 	void Update () {
-		Vector3 p1 = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-		Vector3 p2 = new Vector3(Input.GetAxis("Horizontal2"), Input.GetAxis("Vertical2"));
+		controller = InputManager.ActiveDevice;
+		InputManager.AttachDevice (controller);
+		Vector3 p1 = new Vector3(controller.LeftStickX.Value, controller.LeftStickY.Value);
+		Vector3 p2 = new Vector3(controller.RightStickX.Value, controller.RightStickY.Value);
+
 		float spd = Mathf.Min (p1.magnitude, p2.magnitude);
+
+		controller.Vibrate (p2.magnitude, p1.magnitude);
 
 		transform.position = transform.position + (p1+p2) * spd * speed;
 	}
