065ffa5bdc4ac01fe570889efc7f6bcc03d2bacc
Will McMain <willmcmain@gmail.com>
2016-02-21 23:08:05 +0100

    treasure & mine spawning

--- Assets/Scripts/Blindfield/GameStart.cs
+++ Assets/Scripts/Blindfield/GameStart.cs
@@ -7,11 +7,39 @@ public class GameStart : MonoBehaviour {
 	public GameObject treasure;
 	public GameObject minePrefab;
 
+	public float minTreasureDist = 5.0f;
+	public float minMineDist = 1.0f;
+	public int numMines = 4;
+
 	void Start() {
 		// spawn players
 		lightPlayer.transform.position = RandomPoint(2.0f);
 		heavyPlayer.transform.position = lightPlayer.transform.position + new Vector3 (1.0f, 1.0f);
 
+		// spawn treasure
+		Vector3 treasurePos = RandomPoint();
+		while ((treasurePos - lightPlayer.transform.position).magnitude < minTreasureDist) {
+			treasurePos = RandomPoint ();
+		}
+		treasure.transform.position = treasurePos;
+
+		// spawn mines
+		for (int i = 0; i < numMines; i++) {
+			Vector3 minePos = RandomPoint ();
+			float player1Dist = (lightPlayer.transform.position - minePos).magnitude;
+			float player2Dist = (heavyPlayer.transform.position - minePos).magnitude;
+			float treasureDist = (treasure.transform.position - minePos).magnitude;
+
+			while (player1Dist < minMineDist || player2Dist < minMineDist || treasureDist < minMineDist) {
+				minePos = RandomPoint ();
+				player1Dist = (lightPlayer.transform.position - minePos).magnitude;
+				player2Dist = (heavyPlayer.transform.position - minePos).magnitude;
+				treasureDist = (treasure.transform.position - minePos).magnitude;
+			}
+
+			var mine = GameObject.Instantiate (minePrefab);
+			mine.transform.position = minePos;
+		}
 
 	}
 
